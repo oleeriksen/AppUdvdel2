21beffdd009ac282dfa89de8066979f0600dbcb0
oleeriksen <oe@easv.dk>
2024-10-14 16:30:51 +0200

    init done

---
+++ WebApp1/Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Price must be positive")]
+        public int? Price { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+    }
+}
+

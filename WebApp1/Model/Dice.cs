f246121dd7247ddef9e620267e9b4324a5877eca
oleeriksen <oleeriksen1958@gmail.com>
2024-10-25 10:11:35 +0200

    div

---
+++ WebApp1/Model/Dice.cs
@@ -0,0 +1,31 @@
+namespace WebApp1.Model;
+
+public class Dice
+{
+    private int _eyes;
+
+    private int _size;
+    private Random _random; 
+
+    public Dice()
+    {
+        _random = new Random();
+        _size = 6;
+    }
+
+    public Dice(int size)
+    {
+        _random = new Random();
+        _size = size;
+    }
+
+    public virtual void Roll()
+    {
+        _eyes = _random.Next(_size)+1;
+    }
+
+    public int GetEyes()
+    {
+        return _eyes;
+    }
+}

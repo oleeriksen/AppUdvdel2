4dbf23e93a89783336b701727ab550141230633e
oleeriksen <oleeriksen1958@gmail.com>
2024-11-06 12:56:16 +0100

    div

--- WebApp1/Service/ILoginService.cs
+++ WebApp1/Service/ILoginService.cs
@@ -4,11 +4,11 @@ namespace WebApp1.Service;
 
 public interface ILoginService
 {
-    // return true iff a user is logged in
+    // If a user is logged in, the user will be returned.
+    // If no user is logged in, null will be returned
     Task<User> GetUserLoggedIn();
     
-    // Try to login the user in. If
-    // user is valid the function will return true and the
+    // If user is valid the function will return true and the
     // user is set to be logged in.
     Task<bool> Login(string username, string password);
 }

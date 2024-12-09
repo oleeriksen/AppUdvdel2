00a19042c18716d1c4ad0e1a60248f7223e795e9
oleeriksen <oleeriksen1958@gmail.com>
2024-12-09 12:54:53 +0100

    Update ShoppingRepositoryMongoDB.cs

--- ServerAPI/Repositories/ShoppingRepositoryMongoDB.cs
+++ ServerAPI/Repositories/ShoppingRepositoryMongoDB.cs
@@ -12,7 +12,7 @@ namespace ServerAPI.Repositories
 
         public ShoppingRepositoryMongoDB()
 		{
-            var password = "xl2GH1Uxztjtstwf"; //add
+            var password = ""; //add
             var mongoUri = $"mongodb+srv://olee58:{password}@cluster0.olmnqak.mongodb.net/?retryWrites=true&w=majority";
             
             client = new MongoClient(mongoUri);

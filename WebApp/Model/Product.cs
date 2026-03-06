fbfa050f74f1d50c2510216e030a024ce8586366
oleeriksen <oleeriksen1958@gmail.com>
2026-03-06 13:09:09 +0100

    inputcontrols

---
+++ WebApp/Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Model
+{
+    public class Product
+    {
+            [Required]
+            [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+            public string? Name { get; set; }
+
+            [Required]
+            [Range(1,int.MaxValue, ErrorMessage = "Price must be positive")]
+            public int? Price { get; set; }
+
+            [Required]
+            public string? Description { get; set; }
+        
+            public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+            public bool IsPublished { get; set; }
+
+        }
+    
+}
+

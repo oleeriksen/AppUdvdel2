629c2ffccf50a23e3b8840befebf932f65ab8400
oleeriksen <oleeriksen1958@gmail.com>
2026-03-06 09:31:56 +0100

    shop

---
+++ WebApp/Model/Item.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Model;
+
+public class Item
+{
+    public string Name { get; set; }
+    public double Price { get; set;}
+    public int Stock { get; set; }
+    public string Color { get; set; }
+}
